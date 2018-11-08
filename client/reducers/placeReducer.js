a3587ffefb4adbb5e110f696e34327944d894c93
jdltechworks <jdltechworks@gmail.com>
2018-11-08 08:48:52 +0800

    Initial task 2

--- client/reducers/placeReducer.js
+++ client/reducers/placeReducer.js
@@ -2,11 +2,16 @@ import { handleActions } from 'redux-actions';
 import * as actionTypes from 'actions/placeActionTypes';
 
 const initialState = {
+  cuisines: [],
 };
 const placeReducer = handleActions(
   {
     [actionTypes.SET_DETAILS](state, action) {
       return { ...state, ...action.payload };
+    },
+    [actionTypes.FETCH_CUISINES](state, action) {
+      console.log(action);
+      return { ...state };
     },
   },
   initialState,
--- client/sagas/index.js
+++ client/sagas/index.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 
 import placeSagas from './placeSagas';
+import { getCuisines } from './cuisinesSagas';
 
 export default function* rootSagas() {
   yield all([
     placeSagas(),
+    getCuisines(),
   ]);
 }
