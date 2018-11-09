00454dd21808fef1161515b9bd48fcbfa7778954
jdltechworks <jdltechworks@gmail.com>
2018-11-09 21:24:23 +0800

    Adding google maps feature plus test

--- client/containers/PlaceDetails.js
+++ client/containers/PlaceDetails.js
@@ -4,10 +4,13 @@ import placeActions from 'actions/placeActions';
 import PropTypes from 'prop-types';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Place from 'components/Place/Place';
 import Image from 'components/Image';
 import Map from 'components/Map';
 =======
+=======
+>>>>>>> Adding google maps feature plus test
 import Map from 'components/Map';
 import Details from 'components/Details';
 >>>>>>> Adding google maps feature plus test
@@ -31,9 +34,14 @@ class PlaceDetails extends Component {
       return (
         <Fragment>
 <<<<<<< HEAD
+<<<<<<< HEAD
           <Place place={details} />
           <Map coordinates={details.coor}
           <Image src={details.img} />
+=======
+          <Map coordinates={details.coordinates} />
+          <Details info={details} />
+>>>>>>> Adding google maps feature plus test
 =======
           <Map coordinates={details.coordinates} />
           <Details info={details} />
