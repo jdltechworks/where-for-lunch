0f1accbd5dffb6929a3f17b630eafa0acb532291
jdltechworks <jdltechworks@gmail.com>
2018-11-10 09:58:36 +0800

    Fixing rebase conflicts

--- client/containers/PlaceDetails.js
+++ client/containers/PlaceDetails.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 052bf39cc13657c5022c643af0f5309ae368bc43
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import placeActions from 'actions/placeActions';
@@ -41,52 +36,3 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlaceDetails);
-<<<<<<< HEAD
-=======
-=======
-import { connect } from 'react-redux';
->>>>>>> Adding place details page plus compoenent test
-import React, { Component } from 'react';
-import placeActions from 'actions/placeActions';
-import PropTypes from 'prop-types';
-
-import Details from 'components/Details';
-
-const { placeById } = placeActions;
-
-class PlaceDetails extends Component {
-  static propTypes = {
-    placeById: PropTypes.func,
-    match: PropTypes.object,
-  }
-  componentDidMount() {
-    const { params } = this.props.match;
-    this.props.placeById(params.id);
-  }
-  render() {
-    const { props } = this;
-    const { details } = props;
-
-    if (details) {
-      return (<Details info={details} />);
-    }
-
-    return null;
-  }
-}
-
-<<<<<<< HEAD
-export default PlaceDetails;
->>>>>>> Adding partial implementation for task-3
-=======
-const mapStateToProps = ({ place }) => ({ details: place.details });
-
-
-const mapDispatchToProps = {
-  placeById,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PlaceDetails);
->>>>>>> Adding place details page plus compoenent test
-=======
->>>>>>> 052bf39cc13657c5022c643af0f5309ae368bc43
